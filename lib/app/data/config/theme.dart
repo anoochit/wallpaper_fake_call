33e00c1613b7d0ce55fd38b7431ddf146d974425
Anuchit Chalothorn <anoochit@gmail.com>
2023-01-05 11:09:07 +0700

    use default theme

--- lib/app/data/config/theme.dart
+++ lib/app/data/config/theme.dart
@@ -2,39 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 const menuSpacing = MainAxisAlignment.spaceEvenly;
-const wallpapperTabMenuBackgroundColor = Colors.red;
+const wallpapperTabMenuBackgroundColor = Colors.white;
 
 final themeData = ThemeData(
   // color scheme for whole app
-  primarySwatch: Colors.amber,
+  primarySwatch: Colors.blue,
 
   // background for canvas
-  canvasColor: Colors.green,
+  canvasColor: Colors.white,
 
   // color for button background
   elevatedButtonTheme: const ElevatedButtonThemeData(
     style: ButtonStyle(
       // button background
-      backgroundColor: MaterialStatePropertyAll(Colors.amber),
+      backgroundColor: MaterialStatePropertyAll(Colors.blue),
       // button text color
-      foregroundColor: MaterialStatePropertyAll(Colors.black),
+      foregroundColor: MaterialStatePropertyAll(Colors.white),
     ),
   ),
 
   // drawer
   drawerTheme: const DrawerThemeData(
     // drawer background color
-    backgroundColor: Colors.yellow,
+    backgroundColor: Colors.white,
   ),
 
   // appbar theme
   appBarTheme: const AppBarTheme(
     // background color
-    backgroundColor: Colors.orange,
+    backgroundColor: Colors.blue,
     // text and icon color
-    foregroundColor: Colors.green,
+    foregroundColor: Colors.white,
   ),
 
   // app font style
-  textTheme: GoogleFonts.pacificoTextTheme(),
+  textTheme: GoogleFonts.robotoTextTheme(),
 );

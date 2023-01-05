145b164027ba530e6ec176b6685b499089be4b5b
Anuchit Chalothorn <anoochit@gmail.com>
2023-01-05 09:38:01 +0700

    add pre-config for app theme

--- lib/app/data/config/theme.dart
+++ lib/app/data/config/theme.dart
@@ -1,5 +1,33 @@
 import 'package:flutter/material.dart';
 
-final themeData = ThemeData();
-
 const menuSpacing = MainAxisAlignment.spaceEvenly;
+
+final themeData = ThemeData(
+  // color scheme for whole app
+  primarySwatch: Colors.amber,
+
+  // background for canvas
+  canvasColor: Colors.green,
+
+  // color for button background
+  elevatedButtonTheme: const ElevatedButtonThemeData(
+    style: ButtonStyle(
+      // button background
+      backgroundColor: MaterialStatePropertyAll(Colors.amber),
+      // button text color
+      foregroundColor: MaterialStatePropertyAll(Colors.black),
+    ),
+  ),
+
+  // drawer
+  drawerTheme: const DrawerThemeData(
+    // drawer background color
+    backgroundColor: Colors.yellow,
+  ),
+
+  // appbar theme
+  appBarTheme: const AppBarTheme(
+    // background colors
+    backgroundColor: Colors.orange,
+  ),
+);
